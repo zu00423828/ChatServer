0bf2fd49ea19a2674de1690e10af96e2d7a8d0ab
yuan <zu00423828@gmail.om>
2020-10-28 10:26:59 +0800

    ver1

---
+++ ChatPeer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Photon.SocketServer;
+using PhotonHostRuntimeInterfaces;
+using System.IO;
+namespace ChatServer
+{
+    class ChatPeer : ClientPeer
+    {
+        public Guid Guid { get; private set; }
+
+        private int maxGuessNumber;
+        private int correctNumber;
+        private int remainedChance;
+        private bool alreadySendHelloWorld;
+        public ChatPeer(InitRequest initRequest) : base(initRequest)
+        {
+            Guid = Guid.NewGuid();
+        }
+        private static event Action<ChatPeer, EventData, SendParameters> BroadcastMessage;
+        protected override void OnDisconnect(DisconnectReason reasonCode, string reasonDetail)
+        {
+            ChatServer.Server.ErasePeer(this);
+        }
+
+        protected override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters)
+        {
+            if (operationRequest.OperationCode == 1)
+            {
+                var pa = (string)operationRequest.Parameters[1];
+                var parameter = new Dictionary<byte, object>()
+                {
+                    {(byte)1,pa }
+                };
+                var response = new OperationResponse(operationRequest.OperationCode,parameter);
+                SendOperationResponse(response,new SendParameters());
+                
+            }
+
+        }
+        private void OnBroadcastMessage(ChatPeer peer, EventData eventData, SendParameters sendParameters)
+        {
+            if (peer != this) // do not send chat custom event to peer who called the chat custom operation 
+            {
+                SendEvent(eventData, sendParameters);
+            }
+        }
+
+        public void RefreshGuid()
+        {
+            Guid = Guid.NewGuid();
+        }
+    }
+}
---
+++ ChatServer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Photon.SocketServer;
+using ExitGames.Logging;
+using ExitGames.Logging.Log4Net;
+using log4net.Config;
+using System.IO;
+namespace ChatServer
+{
+    class ChatServer : ApplicationBase
+    {
+        public static ILogger Log { get; }
+        public static ChatServer Server { get; private set; }
+
+        private Dictionary<Guid, ChatPeer> peerDictionary;
+
+        static ChatServer()
+        {
+            Log = LogManager.GetCurrentClassLogger();
+        }
+
+        protected override PeerBase CreatePeer(InitRequest initRequest)
+        {
+            var peer = new ChatPeer(initRequest);
+            while (peerDictionary.ContainsKey(peer.Guid))
+            {
+                peer.RefreshGuid();
+            }
+            peerDictionary.Add(peer.Guid, peer);
+            return peer;
+        }
+
+        protected override void Setup()
+        {
+            log4net.GlobalContext.Properties["Photon:ApplicationLogPath"] = Path.Combine(this.ApplicationPath, "log");
+            FileInfo file = new FileInfo(Path.Combine(this.BinaryPath, "log4net.config"));
+            if (file.Exists)
+            {
+                LogManager.SetLoggerFactory(Log4NetLoggerFactory.Instance);
+                XmlConfigurator.ConfigureAndWatch(file);
+
+                peerDictionary = new Dictionary<Guid, ChatPeer>();
+                Server = this;
+
+                Log.Info("Server Setup Successiful.......");
+            }
+        }
+
+        protected override void TearDown()
+        {
+        }
+        public void ErasePeer(ChatPeer peer)
+        {
+            if (peerDictionary.ContainsKey(peer.Guid))
+            {
+                peerDictionary.Remove(peer.Guid);
+            }
+        }
+    }
+}
